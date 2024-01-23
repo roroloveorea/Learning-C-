2e5c725725db3a24e0ed4195892606cd1d56151e
RX <70211426+roroloveorea@users.noreply.github.com>
2024-01-23 13:51:58 +0800

    Add files via upload

---
+++ TurnBased/TurnBased/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TurnBased;
+
+Unit player = new Unit(100, 20, 12, " player");
+Unit enemy = new Unit(75, 10, 7, "Enemy Mage");
+Random random = new Random();
+int rand;
+while (player.Hp > 0 && enemy.Hp > 0)
+{
+    Console.WriteLine($"{player.UnitName} HP = {player.Hp} , {enemy.UnitName} HP = {enemy.Hp}");
+    Console.WriteLine("Player turn! what will you do");
+    string choice = Console.ReadLine();
+
+    if (choice == "a")
+    {
+        player.Attack(enemy);
+    }
+    else player.Heal();
+    Console.WriteLine("Enemy turn!");
+    rand = random.Next(0, 2);
+    if (rand == 0)
+    {
+        enemy.Attack(player);
+    }
+    else enemy.Heal();
+
+}
+
+if (player.Hp > 0) Console.WriteLine($"{player.UnitName} Won!");
+else Console.WriteLine($"{enemy.UnitName} Won!");
+
---
+++ TurnBased/TurnBased/Unit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnBased
+{
+    internal class Unit
+    {
+        private int currentHp;
+        private int maxHp;
+        private int attackPower;
+        private int healPower;
+        private string unitName;
+        private Random random;
+
+        public int Hp
+        {
+            get { return currentHp; }
+        }
+        public string UnitName
+        {
+            get { return unitName; }
+        }
+        public bool isDead
+        {
+            get { return currentHp <= 0 ; }
+        }
+
+        public Unit(int maxHp, int attackPower, int healPower, string unitName)
+        {
+            this.maxHp = maxHp;
+            this.currentHp = maxHp;
+            this.attackPower = attackPower;
+            this.healPower = healPower;
+            this.unitName = unitName;
+            this.random = new Random();
+        }
+
+        public void Attack(Unit unitToAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int randDamage = (int)(attackPower * rng);
+            unitToAttack.TakeDamage(randDamage);
+            Console.WriteLine($"{unitName} attacks {unitToAttack.unitName} and deals {randDamage} damage!");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHp -= damage;
+            if(isDead)
+                Console.WriteLine($"{UnitName} has been defeated! ");
+        }
+
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healPower);
+            currentHp = heal + currentHp > maxHp ? maxHp : currentHp + heal;
+            Console.WriteLine($"{ UnitName } heals {heal}");
+        }
+    }
+}
